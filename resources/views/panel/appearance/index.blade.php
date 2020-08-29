395b4e9c39bf37214b3407346d3ab244a0f2d22f
Shahariar Ikbal <shahariar.ikbal86@gmail.com>
2020-08-29 12:52:57 +0600

    apparance page done

--- resources/views/panel/appearance/index.blade.php
+++ resources/views/panel/appearance/index.blade.php
@@ -1,10 +1,45 @@
 @extends('layouts.panel')
 
 @section('content')
-    <div class="container-fluid">
-        <div class="col-lg-12">
-            <div class="row">
-                <h1>Hello Appearance........</h1>
+    <div class="container">
+        <div class="row">
+            @include('panel.appearance.nav')
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-body">
+                        <a class="btn btn-default m-b add-page" href="#">Add page</a>
+
+                        <table class="table">
+                            <thead>
+                            <tr>
+                                <th width="45%" class="p-l">Name</th>
+                                <th>Visibility</th>
+                                <th>Public</th>
+                                <th>Last modified</th>
+                                <th></th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr  class="disable-keystroke" >
+                                <td class="p-l">Page Name</td>
+                                <td>
+                                    <div class="setting-switch setting-switch-table">
+                                        <label class="switch">
+                                            <input type="checkbox" class="toggle-page-visibility" name="page_status" id="page_status" value="">
+                                            <span class="slider round"></span>
+                                        </label>
+                                    </div>
+                                </td>
+                                <td>Public</td>
+                                <td>20.20.2020</td>
+                                <td class="p-r text-right">
+                                    <a class="btn btn-default btn-xs" href="#">Edit</a>
+                                </td>
+                            </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
