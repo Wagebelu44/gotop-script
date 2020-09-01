d089652eb978d4bba5f788f4ec72304be42d5a9e
RashiqulRony <rony.mmj@gmail.com>
2020-09-01 11:15:42 +0600

    Provider DB done

---
+++ database/migrations/2020_09_01_051054_create_setting_providers_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSettingProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('setting_providers', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('panel_id');
+            $table->string('domain');
+            $table->string('api_url')->nullable();
+            $table->string('api_key')->nullable();
+            $table->enum('status', ['active', 'inactive'])->default('active');
+            $table->unsignedBigInteger('created_by');
+            $table->unsignedBigInteger('updated_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('setting_providers');
+    }
+}
