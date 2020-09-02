7cd734a9486eda65ebedfb340905431fb1eea6ec
RashiqulRony <rony.mmj@gmail.com>
2020-09-02 09:36:17 +0600

    Bonuse working

---
+++ app/Models/SettingBonuse.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SettingBonuse extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'setting_bonuses';
+
+    protected $fillable = [
+      'panel_id', 'global_payment_method_id', 'bonus_amount', 'deposit_from', 'status', 'created_by', 'updated_by'
+    ];
+}
