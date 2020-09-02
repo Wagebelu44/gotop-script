f65a37cfc6bff94c52506c7b169797ec7ce8ad77
Shahariar Ikbal <shahariar.ikbal86@gmail.com>
2020-09-02 17:08:40 +0600

    module work done

--- app/Http/Controllers/Panel/Setting/ModuleController.php
+++ app/Http/Controllers/Panel/Setting/ModuleController.php
@@ -3,11 +3,62 @@
 namespace App\Http\Controllers\Panel\Setting;
 
 use App\Http\Controllers\Controller;
+use App\Models\SettingModule;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class ModuleController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('panel.settings.modules');
+    }
+
+    public function update(Request $request)
+    {
+        $data = $request->all();
+        $panelId = 1;
+        if ($request->type === 'affiliate') {
+            SettingModule::updateOrCreate(
+                ['panel_id' => $panelId, 'type' => 'affiliate'],
+                [
+                    'commission_rate' => $data['commission_rate'],
+                    'amount'          => $data['amount'],
+                    'approve_payout'  => $data['approve_payout'],
+                    'updated_by'      => auth()->guard('panelAdmin')->id(),
+                ]
+            );
+            return redirect()->back()->with('success', 'Affiliate Setting update successfully!');
+        }
+
+        if ($request->type === 'child_panels') {
+            SettingModule::updateOrCreate(
+                ['panel_id' => $panelId, 'type' => 'child_panels'],
+                [
+                    'amount'          => $data['amount'],
+                    'updated_by'      => auth()->guard('panelAdmin')->id(),
+                ]
+            );
+            return redirect()->back()->with('success', 'Child panels Setting update successfully!');
+        }
+        if ($request->type === 'free_balance') {
+            SettingModule::updateOrCreate(
+                ['panel_id' => $panelId, 'type' => 'free_balance'],
+                [
+                    'amount'          => $data['amount'],
+                    'updated_by'      => auth()->guard('panelAdmin')->id(),
+                ]
+            );
+            return redirect()->back()->with('success', 'Free balance Setting update successfully!');
+        }
+    }
+
+    public function getModuleData(Request $request)
+    {
+        $data = SettingModule::where('panel_id', Auth::user()->panel_id)->where('type', $request->type)->first();
+        return response()->json([
+            'status' => 'success',
+            'data' => $data
+        ], 200);
     }
 }
--- resources/views/panel/settings/modules.blade.php
+++ resources/views/panel/settings/modules.blade.php
@@ -30,7 +30,37 @@
                                         </div>
                                     </td>
                                     <td class="settings-emails-td-actions">
-                                        <a href="javascript:void(0)" class="btn btn-xs btn-default edit-module pull-right" data-toggle="modal" data-target="#cmsModuleEditPopUp" data-title="Affiliate system" data-module="1">
+                                        <a href="javascript:void(0)" class="btn btn-xs btn-default edit-module pull-right" onclick="getModuleData('affiliate')" data-title="Affiliate system" data-module="1">
+                                            Edit
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr class="settings-emails-row">
+                                    <td>
+                                        <div class="settings-emails-row-name">Child panels selling</div>
+                                        <div class="settings-emails-row-description">
+                                            <span>
+                                                A panel with limited features that can have only your panel as a service provider. Users can order child panels on your panel.
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td class="settings-emails-td-actions">
+                                        <a href="javascript:void(0)" class="btn btn-xs btn-default edit-module pull-right" onclick="getModuleData('child_panels')" data-title="Affiliate system" data-module="1">
+                                            Edit
+                                        </a>
+                                    </td>
+                                </tr>
+                                <tr class="settings-emails-row">
+                                    <td>
+                                        <div class="settings-emails-row-name">Free balance</div>
+                                        <div class="settings-emails-row-description">
+                                            <span>
+                                                Set up a one-time free balance amount for new panel users after signing up.
+                                            </span>
+                                        </div>
+                                    </td>
+                                    <td class="settings-emails-td-actions">
+                                        <a href="javascript:void(0)" class="btn btn-xs btn-default edit-module pull-right" onclick="getModuleData('free_balance')" data-title="Affiliate system" data-module="1">
                                             Edit
                                         </a>
                                     </td>
@@ -43,19 +73,34 @@
         </div>
     </div>
 
-    <!--Edit modal-->
-    <div class="modal fade in" id="cmsModuleEditPopUp" tabindex="-1" role="dialog" data-backdrop="static" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+    <!--Edit Affiliate modal-->
+    <div class="modal fade in" id="cmsModuleEditAffiliatePopUp" tabindex="-1" role="dialog" data-backdrop="static" aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-md">
             <div class="modal-content">
-                <form class="form-material" id="moduleEditForm" method="post" action="#" enctype="multipart/form-data">
+                <form class="form-material" id="affiliateModuleEditForm" method="post" action="{{ route('admin.setting.module.update') }}" enctype="multipart/form-data">
                     @csrf
-                    @method('PUT')
-
                     <div class="modal-header">
-                        <h4 class="modal-title" id="ModalLabel">Update menu item</h4>
+                        <h4 class="modal-title" id="ModalLabel">Affiliate system</h4>
                         <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
                     </div>
-                    <div class="modal-body" id="modalBody"></div>
+                    <div class="modal-body" id="modalBody">
+                        <input type="hidden" id="affiliate" name="type" value="affiliate"/>
+                        <div class="form-group">
+                            <label class="form-control-label">Commission rate, %</label>
+                            <input type="number" id="commission_rate" class="form-control" name="commission_rate" required>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label">Minimum payout</label>
+                            <input type="number" id="amount" class="form-control" name="amount" required>
+                        </div>
+                        <div class="form-group">
+                            <label class="form-control-label">Approve payouts</label>
+                            <select class="form-control" name="approve_payout" id="approve_payout" aria-required="true">
+                                <option value="auto">Auto</option>
+                                <option value="manual">Manual</option>
+                            </select>
+                        </div>
+                    </div>
                     <div class="modal-footer">
                         <div class="col-md-6 submit-update-section">
                             <div class="form-actions">
@@ -76,4 +121,110 @@
         </div>
         <!-- /.modal-dialog -->
     </div>
+
+    <!--Edit Child panel modal-->
+    <div class="modal fade in" id="cmsModuleEditChildPopUp" tabindex="-1" role="dialog" data-backdrop="static" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <form class="form-material" id="childModuleEditForm" method="post" action="{{ route('admin.setting.module.update') }}" enctype="multipart/form-data">
+                    @csrf
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="ModalLabel">Child panels selling</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    </div>
+                    <div class="modal-body" id="modalBody">
+                        <input type="hidden" id="child_panels" name="type" value="child_panels"/>
+                        <div class="form-group">
+                            <label class="form-control-label">Price per month</label>
+                            <input type="number" id="month_amount" class="form-control" name="amount" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="col-md-6 submit-update-section">
+                            <div class="form-actions">
+                                <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> Update</button>
+                                <button type="button" class="btn btn-danger waves-effect text-left" data-dismiss="modal" >Close</button>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-actions" id="deactive-btn" style="display: none">
+                                <input type="hidden" id="edit_id" value=""/>
+                                <a class="btn btn-default waves-effect text-left" data-toggle="confirmation" data-original-title="" title="">Deactivate</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+
+    <!---Free balance modal--->
+    <div class="modal fade in" id="cmsModuleEditFreeBalancePopUp" tabindex="-1" role="dialog" data-backdrop="static" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-md">
+            <div class="modal-content">
+                <form class="form-material" id="freeBalanceModuleEditForm" method="post" action="{{ route('admin.setting.module.update') }}" enctype="multipart/form-data">
+                    @csrf
+
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="ModalLabel">Free Balance</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    </div>
+                    <div class="modal-body" id="modalBody">
+                        <input type="hidden" id="free_balance" name="type" value="free_balance"/>
+                        <div class="form-group">
+                            <label class="form-control-label">Amount</label>
+                            <input type="number" id="free_amount" class="form-control" name="amount" required>
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <div class="col-md-6 submit-update-section">
+                            <div class="form-actions">
+                                <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> Update</button>
+                                <button type="button" class="btn btn-danger waves-effect text-left" data-dismiss="modal" >Close</button>
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-actions" id="deactive-btn" style="display: none">
+                                <input type="hidden" id="edit_id" value=""/>
+                                <a class="btn btn-default waves-effect text-left" data-toggle="confirmation" data-original-title="" title="">Deactivate</a>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </div>
+            <!-- /.modal-content -->
+        </div>
+        <!-- /.modal-dialog -->
+    </div>
+@endsection
+@section('scripts')
+<script>
+    function getModuleData(type) {
+        $.ajax({
+            type: "POST",
+            dataType: "json",
+            url: 'module-edit',
+            data: {'type': type, "_token": "{{ csrf_token() }}"},
+            success(response) {
+                console.log(response)
+                if (type == 'affiliate') {
+                    $('#cmsModuleEditAffiliatePopUp').modal('show')
+                    $('#commission_rate').val(response.data.commission_rate);
+                    $('#amount').val(response.data.amount);
+                    $('#approve_payout').val(response.data.approve_payout);
+                }
+                if (type == 'child_panels') {
+                    $('#cmsModuleEditChildPopUp').modal('show')
+                    $('#month_amount').val(response.data.amount);
+                }
+                if (type == 'free_balance') {
+                    $('#cmsModuleEditFreeBalancePopUp').modal('show')
+                    $('#free_amount').val(response.data.amount);
+                }
+            }
+        })
+    }
+</script>
 @endsection
