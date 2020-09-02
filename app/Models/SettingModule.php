65d2efe3e79f7196ac0d0d8c0f279c3dc51ef645
RashiqulRony <rony.mmj@gmail.com>
2020-09-02 12:28:50 +0600

    Module DB added

---
+++ app/Models/SettingModule.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class SettingModule extends Model
+{
+    use SoftDeletes;
+
+    protected $table = ['setting_modules'];
+
+    protected $fillable = [
+        'panel_id', 'amount', 'commission_rate', 'approve_payout', 'title', 'description', 'type', 'status', 'updated_by', 'created_by',
+    ];
+}
