d630b3ad82fced1f6fb2b606c615c6b5c1fbef0d
Shahariar Ikbal <shahariar.ikbal86@gmail.com>
2020-09-02 11:29:44 +0600

    bonus crud done

--- resources/views/panel/settings/bonuses.blade.php
+++ resources/views/panel/settings/bonuses.blade.php
@@ -1,6 +1,9 @@
 @extends('layouts.panel')
 
 @section('content')
+    @php
+        $resource = 'admin.setting.bonuses.';
+    @endphp
     <div class="row all-mt-30">
         @include('panel.settings.navbar')
 
@@ -21,16 +24,46 @@
                         </tr>
                         </thead>
                         <tbody>
+                        @foreach($bonuses as $key => $bonuse)
                             <tr>
-                                <td class="p-l">1</td>
-                                <td>100%</td>
-                                <td>PayOP</td>
-                                <td>100.00</td>
-                                <td>Enabled</td>
+                                <td class="p-l">{{ $key+1 }}</td>
+                                <td>
+                                    @if(isset($bonuse->bonus_amount))
+                                        {{ $bonuse->bonus_amount }}%
+                                    @endif
+                                </td>
+                                <td>
+                                    @if($bonuse->global_payment_method_id === 1)
+                                        Paypal
+                                    @elseif($bonuse->global_payment_method_id === 2)
+                                        PayoP
+                                    @elseif($bonuse->global_payment_method_id === 3)
+                                        Coinbase
+                                    @elseif($bonuse->global_payment_method_id === 4)
+                                        Bank/Other
+                                    @else
+                                        Payoneer
+                                    @endif
+                                </td>
+                                <td>
+                                    @if( $bonuse->deposit_from )
+                                        {{ $bonuse->deposit_from }}
+                                    @endif
+                                </td>
+                                <td>
+                                    @if( $bonuse->status === 'active' )
+                                        Enabled
+                                    @else
+                                        Disabled
+                                    @endif
+                                </td>
                                 <td class="p-r text-right">
-                                    <a class="btn btn-default btn-xs" href="javascript:void(0)" data-toggle="modal" data-target="#cmsBonusesEditPopUp">Edit</a>
+                                    <button data-url="{{ route($resource.'edit', $bonuse->id) }}" data-id="{!! $bonuse->id !!}" class="edit btn btn-default m-t-20">
+                                        Edit
+                                    </button>
                                 </td>
                             </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
@@ -41,7 +74,7 @@
     <div class="modal fade in" id="cmsBonusesPopUp" tabindex="-1" role="dialog" data-backdrop="static" aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-md">
             <div class="modal-content">
-                <form class="form-material" id="menuForm" method="post" action="" enctype="multipart/form-data">
+                <form class="form-material" id="menuForm" method="post" action="{{ route($resource.'store') }}" enctype="multipart/form-data">
                     @csrf
                     <div class="modal-header">
                         <h4 class="modal-title" id="myLargeModalLabel">Add bonus</h4>
@@ -64,8 +97,13 @@
 
                             <div class="form-group">
                                 <label for="menu_link" class="control-label" for="payment_method_id">For method</label>
-                                <select class="form-control" id="payment_method_id" name="payment_method_id" aria-required="true">
+                                <select class="form-control" id="payment_method_id" name="global_payment_method_id" aria-required="true">
                                     <option value="">Select payment method</option>
+                                    <option value="1">Paypal</option>
+                                    <option value="2">PayoP</option>
+                                    <option value="3">Coinbase</option>
+                                    <option value="4">Bank/Other</option>
+                                    <option value="5">Payoneer</option>
                                 </select>
                                 @error('payment_method_id')
                                 <span role="alert">
@@ -87,8 +125,8 @@
                             <div class="form-group">
                                 <label class="control-label" for="status">Status</label>
                                 <select class="form-control" name="status" id="status" aria-required="true">
-                                    <option value="0" {{ old('status') == '0' ? 'selected' : '' }}>Disabled</option>
-                                    <option value="1" {{ old('status') == '1' ? 'selected' : '' }}>Enabled</option>
+                                    <option value="active">Active</option>
+                                    <option value="inactive">Inactive</option>
                                 </select>
                                 @error('status')
                                 <span role="alert">
@@ -102,7 +140,7 @@
 
                     <div class="modal-footer">
                         <div class="form-actions">
-                            <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> Save Change</button>
+                            <button type="submit" class="btn btn-success"> <i class="fa fa-check"></i> Save</button>
                         </div>
                         <button type="button" class="btn btn-danger waves-effect text-left" data-dismiss="modal">Close</button>
                     </div>
@@ -119,61 +157,98 @@
     <div class="modal fade in" id="cmsBonusesEditPopUp" tabindex="-1" role="dialog" data-backdrop="static" aria-labelledby="myLargeModalLabel" aria-hidden="true">
         <div class="modal-dialog modal-md">
             <div class="modal-content">
-                <div class="modal-header">
-                    <h4 class="modal-title" id="myEditModalLabel">Update bonus</h4>
-                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
-                </div>
-                <div class="modal-body">
+                <form class="form-material" id="editFormBonus" method="post" action="">
+                    @method('put')
+                    @csrf
+                    <div class="modal-header">
+                        <h4 class="modal-title" id="myEditModalLabel">Update bonus</h4>
+                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button>
+                    </div>
                     <div class="modal-body">
-                        <div class="form-group form-group__languages">
-                            <label class="control-label" for="edit_bonus_amount">Bonus amount</label>
-                            <div class="input-group">
-                                <input type="number" id="edit_bonus_amount" class="form-control" name="edit_bonus_amount" min="0.01" step="0.01" max="100" value="" aria-required="true">
-                                <div class="input-group-addon">%</div>
-                            </div>
-                            <span role="alert">
+                        <div class="modal-body">
+                            <div class="form-group form-group__languages">
+                                <label class="control-label" for="edit_bonus_amount">Bonus amount</label>
+                                <div class="input-group">
+                                    <input type="number" id="edit_bonus_amount" class="form-control" name="bonus_amount" min="0.01" step="0.01" max="100" value="" aria-required="true">
+                                    <div class="input-group-addon">%</div>
+                                </div>
+                                <span role="alert">
                                 <strong id="error_edit_bonus_amount"></strong>
                             </span>
-                        </div>
-                        <div class="form-group">
-                            <label for="menu_link" class="control-label" for="edit_payment_method_id">For method</label>
-                            <select class="form-control" id="edit_payment_method_id" name="edit_payment_method_id" aria-required="true">
-                                <option value="">Select payment method</option>
-                            </select>
-                            <span role="alert">
+                            </div>
+                            <div class="form-group">
+                                <label for="menu_link" class="control-label" for="edit_payment_method_id">For method</label>
+                                <select class="form-control" id="edit_payment_method_id" name="global_payment_method_id" aria-required="true">
+                                    <option value="">Select payment method</option>
+                                    <option value="1">Paypal</option>
+                                    <option value="2">PayoP</option>
+                                    <option value="3">Coinbase</option>
+                                    <option value="4">Bank/Other</option>
+                                    <option value="5">Payoneer</option>
+                                </select>
+                                <span role="alert">
                                 <strong id="error_edit_payment_method_id"></strong>
                             </span>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="edit_deposit_from">Deposit from</label>
-                            <input type="number" class="form-control" name="edit_deposit_from" id="edit_deposit_from" min="0.01" step="0.01" max="100" value="" aria-required="true">
-                            <span role="alert">
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label" for="edit_deposit_from">Deposit from</label>
+                                <input type="number" class="form-control" name="deposit_from" id="edit_deposit_from" min="0.01" step="0.01" max="100" value="" aria-required="true">
+                                <span role="alert">
                                 <strong id="error_edit_deposit_from"></strong>
                             </span>
-                        </div>
-                        <div class="form-group">
-                            <label class="control-label" for="status">Status</label>
-                            <select class="form-control" name="edit_status" id="edit_status" aria-required="true">
-                                <option value="0">Disabled</option>
-                                <option value="1">Enabled</option>
-                            </select>
-                            <span role="alert">
+                            </div>
+                            <div class="form-group">
+                                <label class="control-label" for="status">Status</label>
+                                <select class="form-control" name="status" id="edit_status" aria-required="true">
+                                    <option value="active">Active</option>
+                                    <option value="inactive">Inactive</option>
+                                </select>
+                                <span role="alert">
                                 <strong id="error_status"></strong>
                             </span>
+                            </div>
                         </div>
+                        <input type="hidden" id="edit_bonus_id"/>
                     </div>
-                    <input type="hidden" id="edit_bonus_id"/>
-                </div>
-                <div class="modal-footer">
-                    <div class="form-actions">
-                        <a href="javascript:void(0)" class="btn btn-success"> <i class="fa fa-check"></i> Save Change</a>
+                    <div class="modal-footer">
+                        <div class="form-actions">
+                            <button type="button" onclick="document.getElementById('editFormBonus').submit();" class="btn btn-primary"> <i class="fa fa-check"></i> Update Provider</button>
+                        </div>
+                        <button type="button" class="btn btn-danger waves-effect text-left" data-dismiss="modal">Close</button>
                     </div>
-                    <button type="button" class="btn btn-danger waves-effect text-left" data-dismiss="modal">Close</button>
-                </div>
+                </form>
             </div>
             <!-- /.modal-content -->
         </div>
         <!-- /.modal-dialog -->
     </div>
     <!--End:Edit Modal-->
+@endsection
+@section('scripts')
+    <script>
+
+        $(document).on('click', '.edit', function (e) {
+            e.preventDefault();
+            let url = $(this).attr('data-url');
+            let id = $(this).attr('data-id');
+            $.ajax({
+                url:url,
+                type:"GET",
+                dataType:"JSON",
+                success(response) {
+
+                    if (response.status === 'success'){
+                        $('#cmsBonusesEditPopUp').modal('show');
+                        $("#edit_bonus_amount").val(response.data.bonus_amount);
+                        $("#edit_payment_method_id").val(response.data.global_payment_method_id);
+                        $("#edit_deposit_from").val(response.data.deposit_from);
+                        document.forms['editFormBonus'].elements['status'].value = response.data.status;
+                        let updateUrl = "{{ url('admin/setting/bonuses') }}/"+id;
+                        $('#editFormBonus').attr('action', updateUrl);
+                    }
+
+                }
+            })
+        })
+    </script>
 @endsection
