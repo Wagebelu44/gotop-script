279422bb6edc93500b41798e1e776eb9b6d5db70
Atik Bin Hashmee <atikhashmee6235@gmail.com>
2020-09-05 19:45:26 +0600

    user model

--- app/User.php
+++ app/User.php
@@ -2,23 +2,37 @@
 
 namespace App;
 
+use Illuminate\Notifications\Notifiable;
+use Spatie\Activitylog\Traits\LogsActivity;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
-    use Notifiable;
+    use Notifiable, LogsActivity;
 
+    protected $table = 'users';
     /**
      * The attributes that are mass assignable.
      *
      * @var array
      */
-    /* protected $fillable = [
-        'panel_id', 'name', 'email', 'password',
-    ]; */
-    protected $guarded = [];
+    protected $fillable = [
+        'panel_id'
+        , 'username'
+        , 'skype_name'
+        , 'phone'
+        , 'balance'
+        , 'email'
+        , 'api_key'
+        , 'referral_key'
+        , 'email_verified_at'
+        , 'password'
+        , 'status'
+        , 'status'
+    ];
+    //protected $guarded = [];
 
     /**
      * The attributes that should be hidden for arrays.
@@ -37,4 +51,22 @@ class User extends Authenticatable implements MustVerifyEmail
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+  
+
+    protected static $logAttributes = ['*'];
+    protected static $logOnlyDirty = true;
+    protected static $submitEmptyLogs = false;
+    protected static $logName = 'User'; //custom_log_name_for_this_model
+
+    public function getDescriptionForEvent(string $eventName): string
+    {
+        return self::$logName. " {$eventName}";
+    }
+
+    public function tapActivity(Activity $activity)
+    {
+        $activity->ip = \request()->ip();
+        $activity->panel_id = auth()->user()->panel_id;
+    }
 }
