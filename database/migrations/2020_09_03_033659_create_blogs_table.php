b98a24b540d5333d725fa3609f54f63dd9e5284a
Sudip <palash.sudip@gmail.com>
2020-09-16 13:50:49 +0600

    blog

--- database/migrations/2020_09_03_033659_create_blogs_table.php
+++ database/migrations/2020_09_03_033659_create_blogs_table.php
@@ -24,7 +24,7 @@ class CreateBlogsTable extends Migration
             $table->string('meta_title')->nullable();
             $table->string('meta_keyword')->nullable();
             $table->text('meta_description')->nullable();
-            $table->enum('type', ['blog', 'trending_blog', 'popular_blog'])->default('blog');
+            $table->enum('type', ['Blog', 'Trending', 'Popular'])->default('Blog');
             $table->enum('status', ['Active', 'Deactivated'])->default('Active');
             $table->unsignedBigInteger('created_by')->nullable();
             $table->unsignedBigInteger('updated_by')->nullable();
