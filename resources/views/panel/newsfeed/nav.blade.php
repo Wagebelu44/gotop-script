14bfba54f52fc2eed51ccc3bd51f66c4f54c15a8
Sudip <palash.sudip@gmail.com>
2020-09-26 18:55:50 +0600

    temp

--- resources/views/panel/newsfeed/nav.blade.php
+++ resources/views/panel/newsfeed/nav.blade.php
@@ -1,6 +1,6 @@
 <div class="col-md-3">
     <ul class="list-group">
-        <a class="list-group-item {{ Request::segment(2) == 'newsfeed' ? 'active' : '' }}" href="{{ route('admin.newsfeed.index') }}">Newsfeed</a>
-        <a class="list-group-item {{ Request::segment(2) == 'newsfeed-category' ? 'active' : '' }}" href="{{ route('admin.newsfeed-category.index') }}">Blog Categories</a>
+        <a class="list-group-item {{ Request::segment(2) == 'newsfeed' ? 'active' : '' }}" href="{{ route('admin.newsfeed.index') }}">News Feed</a>
+        <a class="list-group-item {{ Request::segment(2) == 'newsfeed-category' ? 'active' : '' }}" href="{{ route('admin.newsfeed-category.index') }}">News Categories</a>
     </ul>
 </div>
