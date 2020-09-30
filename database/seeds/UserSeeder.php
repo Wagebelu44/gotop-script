9261004f5b86c8fe381f41b97efd6fe774989c43
Sudip <palash.sudip@gmail.com>
2020-09-30 20:50:19 +0600

    fix code

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -16,12 +16,14 @@ class UserSeeder extends Seeder
         for ($i=0; $i <100 ; $i++) {
             DB::table('users')->insert([
                 'panel_id' => 1,
+                'uuid' => Str::uuid(),
                 'name' => 'User '.$i,
                 'username' => 'user_'.$i,
                 'skype_name' => 'User_'.$i,
                 'email' => 'user'.$i.'@gmail.com',
                 'password' => bcrypt('12345678'),         
                 'api_key'  => Str::random(18),
+                'referral_key' => substr(md5(microtime()), 0, 6),
                 'status' => 'Active',
                 'last_login_at' => now(),
             ]);
