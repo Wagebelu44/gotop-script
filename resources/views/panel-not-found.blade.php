2a84004504073174d7341cb6441eaa1bd53b776d
Sudip <palash.sudip@gmail.com>
2020-10-14 17:38:12 +0600

    panel added

--- resources/views/panel-not-found.blade.php
+++ resources/views/panel-not-found.blade.php
@@ -8,7 +8,7 @@
                 <div class="card mb-3 text-center border-0">
                     <div class="card-body">
                         <p class="card-text mb-3"><span class="firstLine">{{ $msg }} </p>
-                        <a href="http://gopanelshop.com" class="btn btn-success btn-lg">Order your panel</a>
+                        <a href="http://gopanelshop.com" class="btn btn-success btn-lg">Order new panel</a>
                     </div>
                 </div>
             </div>
