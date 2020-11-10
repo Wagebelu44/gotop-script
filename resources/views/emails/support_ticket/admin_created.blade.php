9b292e3a4aa1a5acf1feea3740c3884ef3865739
Sudip Palash <palash.sudip@gmail.com>
2020-11-10 14:42:03 +0600

    issue fix

--- resources/views/emails/support_ticket/admin_created.blade.php
+++ resources/views/emails/support_ticket/admin_created.blade.php
@@ -4,7 +4,7 @@
 <b>Username:</b> {{ ucfirst($ticket['username'] ) }}<br>
 <b>Subject:</b> {{ ucfirst($ticket['subject'] ) }}<br>
 <b>Request:</b> {{ ucfirst($ticket['subject'] ) }}<br>
-<b>Description:</b> {{$ticket['description'] }}<br>
+<b>Description:</b> {!! $ticket['description'] !!}<br>
 
 
 Thanks,<br>
--- resources/views/emails/support_ticket/created.blade.php
+++ resources/views/emails/support_ticket/created.blade.php
@@ -5,7 +5,7 @@
 <b>Username:</b> {{ ucfirst($s_ticket->sender->username) }}<br>
 <b>Subject:</b> {{ ucfirst($s_ticket->subject) }}<br>
 <b>Request:</b> {{ ucfirst($s_ticket->subject) }}<br>
-<b>Description:</b> {{$s_ticket->description}}<br>
+<b>Description:</b> {!! $s_ticket->description !!}<br>
 
 Thanks,<br>
 {{ config('app.name') }}
